33e8e092c25f9fd1feb2c1178823bc900e2ba5ba
Mayur Dodake <mayurdodake7@gmail.com>
2024-11-09 16:27:46 +0530

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by Mayur on 09/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
